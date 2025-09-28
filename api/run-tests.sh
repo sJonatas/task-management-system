b3229b4aa70b99db20330be953092223bd596135
sjonatas <jonatas094@gmail.com>
2025-09-28 15:56:51 -0300

    removing comments

--- api/run-tests.sh
+++ api/run-tests.sh
@@ -1,3 +1,1 @@
-#docker exec -it php-laravel-api /bin/bash -c 'mysql -u user -ppasspwd -e "CREATE DATABASE IF NOT EXISTS task_manager_testing"'
-
 docker exec -it php-laravel-api /bin/bash -c 'php artisan test --coverage --coverage-html=coverage'
